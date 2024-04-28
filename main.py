5f5f04912f383462bee98cfcd242c353f2d08ad3
jasper_a <andrea.nicole.jasper@gmail.com>
2024-04-28 13:01:02 -0600

    adds additional characters to animaiton

--- main.py
+++ main.py
@@ -27,20 +27,26 @@ def scale_image(image, scale):
     h = image.get_height()
     return pygame.transform.scale(image, (w * scale, h * scale))
 
+# load character images
+mob_animations = []
+mob_types = ["elf", "imp", "skeleton", "goblin", "muddy", "tiny_zombie", "big_demon"]
+
 animation_types = ["idle", "run"]
-#load images
-animation_list = []
-for animation in animation_types:
-    #reset temporary list of images
-    temp_list = []
-    for i in range(4):
-        img = pygame.image.load(f"assets/images/characters/elf/{animation}/{i}.png").convert_alpha()
-        img = scale_image(img, constants.SCALE)
-        temp_list.append(img)
-    animation_list.append(temp_list)
+for mob in mob_types:
+    #load images
+    animation_list = []
+    for animation in animation_types:
+        #reset temporary list of images
+        temp_list = []
+        for i in range(4):
+            img = pygame.image.load(f"assets/images/characters/{mob}/{animation}/{i}.png").convert_alpha()
+            img = scale_image(img, constants.SCALE)
+            temp_list.append(img)
+        animation_list.append(temp_list)
+    mob_animations.append(animation_list)
 
 #create player
-player = Character(100, 100, animation_list)
+player = Character(100, 100, mob_animations, 0)
 
 #main game loop
 run = True
@@ -68,7 +74,7 @@ while run:
 
     #update player
     player.update()
-    
+
     #draw player on screen
     player.draw(screen)
 
@@ -95,7 +101,7 @@ while run:
             if event.key == pygame.K_w:
                 moving_up = False
             if event.key == pygame.K_s:
-                moving_down = False 
+                moving_down = False
 
     pygame.display.update()
 
